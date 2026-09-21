205355952d20c38f2bd485d06536618e587ca973
Tanya <tanyalvrnnk@gmail.com>
2026-09-21 14:39:16 -0400

    Complete homework 3

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::io;
+use std::io::{self, Read};
 fn main() {
     let mut inp = String::new();
-    io::stdin().read_line(&mut inp).unwrap();
+    io::stdin().read_to_string(&mut inp).unwrap();
     let words: Vec<i64> = inp.split_whitespace().map(|s| s.parse().unwrap()).collect();
     println!("{}", words.iter().sum::<i64>());
 }
